e265b0a41a42db56bf49f7b4f5abfa63af875a63
Sahil <78251568+sahilsajan@users.noreply.github.com>
2021-09-08 12:43:54 +0530

    Add files via upload

---
+++ Assignments/Patterns/Pattern3.java
@@ -0,0 +1,17 @@
+package patterns;
+//Q-  *****
+//    ****
+//    ***
+//    **
+//    *
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 4; i++){
+            for(int j = 5-i; j > 0; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
