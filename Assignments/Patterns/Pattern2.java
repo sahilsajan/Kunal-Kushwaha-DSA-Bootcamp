6cb8edae3693f619c93d21d11baf961456f36508
Sahil <78251568+sahilsajan@users.noreply.github.com>
2021-09-08 12:40:38 +0530

    Add files via upload

---
+++ Assignments/Patterns/Pattern2.java
@@ -0,0 +1,17 @@
+package patterns;
+//Q-  *
+//    **
+//    ***
+//    ****
+//    *****
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 4; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
