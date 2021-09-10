dbff0e1946431936be2d3320624b0cacf60f114a
Sahil <78251568+sahilsajan@users.noreply.github.com>
2021-09-10 19:40:03 +0530

    Update Pattern3.java

--- Assignments/Patterns/Pattern3.java
+++ Assignments/Patterns/Pattern3.java
@@ -7,8 +7,8 @@ package patterns;
 
 public class Pattern3 {
     public static void main(String[] args) {
-        for(int i = 0; i <= 4; i++){
-            for(int j = 5-i; j > 0; j--){
+        for(int i = 1; i <= 5; i++){
+            for(int j = 6-i; j > 0; j--){
                 System.out.print("*");
             }
             System.out.println();
